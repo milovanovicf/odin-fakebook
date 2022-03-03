8c8df5d7dc489054daccfad25978208d1f2b1be3
Filip <milovanovicfilip007@gmail.com>
2022-03-03 18:13:21 +0100

    initial commit
    Generated new rails project, add Users, Posts and Comments.

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :comments
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "posts#index"
+end
