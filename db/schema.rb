3654a1adc36ba18b68478ac678cb6a135c2b7083
Filip <milovanovicfilip007@gmail.com>
2022-03-09 17:22:28 +0100

    Added likes to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_224914) do
+ActiveRecord::Schema.define(version: 2022_03_09_161815) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -68,6 +68,5 @@ ActiveRecord::Schema.define(version: 2022_03_07_224914) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "likes", "posts", column: "likeable_id"
   add_foreign_key "likes", "users"
 end
